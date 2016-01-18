7943c3de693abbd6aec5a440a5408914b58c1157
zhaokuo <1014336691@qq.com>
2016-01-18 23:38:51 +0800

    添加注册窗口, 完成基本布局设计

--- WindowsClint/src/LoginWindow.java
+++ WindowsClint/src/LoginWindow.java
@@ -8,11 +8,12 @@ public class LoginWindow extends JFrame {
     //控件列表
     JLabel lblUser = new JLabel("用户名: ");
     JLabel lblPsw = new JLabel("密    码: ");
+    JLabel lblSigin = new JLabel("新用户?点击注册");
+    JLabel lblFindPsw = new JLabel("密码找回");
     JTextField txtfUser = new JTextField(25);
     JPasswordField txtfPsw = new JPasswordField(25);
     JButton btnLogin = new JButton("登录");
     JButton btnExit = new JButton("退出");
-    JLabel lblSigin = new JLabel("新用户?点击注册");
     Box bxUser = Box.createHorizontalBox();
     Box bxPsw = Box.createHorizontalBox();
     Box bxBtn = Box.createHorizontalBox();
@@ -29,11 +30,9 @@ public class LoginWindow extends JFrame {
         Dimension dim = tool.getScreenSize();
         int w = (int)dim.getWidth();
         int h = (int)dim.getHeight();
-        this.setBounds(w/2-250,h/2-150,300,200);
+        this.setBounds(w/2-250,h/2-150,300,250);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //设置属性
-        txtfUser.setSize(16,50);
-        txtfPsw.setSize(16,50);
 
         //设置布局
 
@@ -49,22 +48,26 @@ public class LoginWindow extends JFrame {
         bxPsw.add(Box.createHorizontalStrut(50));
 
         bxBtn.add(btnLogin);
-        bxBtn.add(Box.createHorizontalStrut(10));
+        bxBtn.add(Box.createHorizontalStrut(15));
         bxBtn.add(btnExit);
+
 
         bxAll.add(Box.createVerticalStrut(50));
         bxAll.add(bxUser);
         bxAll.add(Box.createVerticalStrut(10));
         bxAll.add(bxPsw);
+        bxAll.add(lblFindPsw);
         bxAll.add(Box.createVerticalStrut(30));
         bxAll.add(bxBtn);
         bxAll.add(Box.createVerticalStrut(30));
-        bxAll.add(lblSigin);
 
         this.add(bxAll);
+        lblSigin.setHorizontalAlignment(SwingConstants.RIGHT);
+        this.add(lblSigin,BorderLayout.SOUTH);
     }
 
     public static void main(String[] args){
-        new LoginWindow();
+        new SigninWindow();
+      //  new LoginWindow();
     }
 }
---
+++ WindowsClint/src/SigninWindow.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by zhaokuo on 2016/01/18.
+ */
+public class SigninWindow extends JFrame {
+    JLabel lblEmail =  new JLabel("邮    箱: ");
+    JLabel lblUser =  new JLabel("用户名: ");
+    JLabel lblPsw =  new JLabel("密    码: ");
+    JLabel lblNotiU = new JLabel("*");
+    JLabel lblNotiP = new JLabel("*");
+    JLabel lblNotiE = new JLabel("*");
+    JTextField txtEmail = new JTextField();
+    JTextField txtUser = new JTextField();
+    JTextField txtPsw = new JTextField();
+    JButton btnOK = new JButton("确定");
+    JButton btnCancle = new JButton("取消");
+    Box bxEmail = Box.createHorizontalBox();
+    Box bxUser = Box.createHorizontalBox();
+    Box bxPsw = Box.createHorizontalBox();
+    Box bxBtn = Box.createHorizontalBox();
+    Box bxAll = Box.createVerticalBox();
+    public SigninWindow(){
+        bxUser.add(Box.createHorizontalStrut(50));
+        bxUser.add(lblUser);
+        bxUser.add(txtUser);
+        bxUser.add(lblNotiU);
+        bxUser.add(Box.createHorizontalStrut(50));
+        bxPsw.add(Box.createHorizontalStrut(50));
+        bxPsw.add(lblPsw);
+        bxPsw.add(txtPsw);
+        bxPsw.add(lblNotiP);
+        bxPsw.add(Box.createHorizontalStrut(50));
+        bxEmail.add(Box.createHorizontalStrut(50));
+        bxEmail.add(lblEmail);
+        bxEmail.add(txtEmail);
+        bxEmail.add(lblNotiE);
+        bxEmail.add(Box.createHorizontalStrut(50));
+        bxBtn.add(btnOK);
+        bxBtn.add(Box.createHorizontalStrut(20));
+        bxBtn.add(btnCancle);
+
+        bxAll.add(Box.createVerticalStrut(80));
+        bxAll.add(bxUser);
+        bxAll.add(Box.createVerticalStrut(15));
+        bxAll.add(bxPsw);
+        bxAll.add(Box.createVerticalStrut(15));
+        bxAll.add(bxEmail);
+        bxAll.add(Box.createVerticalStrut(15));
+        bxAll.add(bxBtn);
+        bxAll.add(Box.createVerticalStrut(30));
+        this.add(bxAll);
+
+        Toolkit tool = Toolkit.getDefaultToolkit();
+        Dimension dim = tool.getScreenSize();
+        int w = (int)dim.getWidth();
+        int h = (int)dim.getHeight();
+        this.setBounds((w-300)/2,(h-300)/2,300,300);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
